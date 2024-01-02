ab101f2e19de8186cc86095bc4708b02131189db
xLeilu <maciekpuszka@gmail.com>
2024-01-02 15:40:46 +0100

    init base app

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text("Library App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Strona główna aplikacji',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
