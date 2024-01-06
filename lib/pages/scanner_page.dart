949ffdd2ac443ffd1ac2e33b1f065779ee0e3181
xLeilu <71885335+xLeilu@users.noreply.github.com>
2024-01-06 18:31:25 +0100

    feat: implemented barcode scanner (#3)

--- lib/pages/scanner_page.dart
+++ lib/pages/scanner_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ScannerPage extends StatefulWidget {
   const ScannerPage({super.key});
@@ -8,6 +10,18 @@ class ScannerPage extends StatefulWidget {
 }
 
 class _ScannerPageState extends State<ScannerPage> {
+  String barcode = '';
+
+  _launchGoogleSearch(String query) async {
+    String url = 'https://www.google.com/search?q=$query';
+
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -33,6 +47,29 @@ class _ScannerPageState extends State<ScannerPage> {
               children: <Widget>[
                 const Text(
                   'Strona skanera',
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    var res = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              const SimpleBarcodeScannerPage(),
+                        ));
+                    setState(() {
+                      if (res is String) {
+                        barcode = res;
+                      }
+                    });
+                  },
+                  child: const Text('Open Scanner'),
+                ),
+                Text('Barcode Result: $barcode'),
+                ElevatedButton(
+                  onPressed: () {
+                    _launchGoogleSearch(barcode);
+                  },
+                  child: Text('Search on Google'),
                 ),
               ],
             ),
